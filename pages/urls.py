431314b485f1929fd258422c9f35ab9e4dcb513a
Andres <asuarezr2@eafit.edu.co>
2025-02-11 13:31:59 -0500

    Activity_3

--- pages/urls.py
+++ pages/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView, ProductIndexView
+from .views import HomePageView, AboutPageView, ProductIndexView, ProductShowView, ContactPageView
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
     path('products/', ProductIndexView.as_view(), name='index'),
-    path('products/<str:id>', ProductIndexView.as_view(), name='show'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('contact/', ContactPageView.as_view(), name='contact'),  # Nueva ruta agregada
 ]
--- pages/views.py
+++ pages/views.py
@@ -22,10 +22,10 @@ class AboutPageView(TemplateView):
     
 class Product:
     products = [
-    {"id":"1", "name":"TV", "description":"Best TV"},
-    {"id":"2", "name":"iPhone", "description":"Best iPhone"},
-    {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
-    {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+    {"id":"1", "name":"TV", "description":"Best TV", "price": 500},
+    {"id":"2", "name":"iPhone", "description":"Best iPhone", "price": 999},
+    {"id":"3", "name":"Chromecast", "description":"Best Chromecast", "price": 50},
+    {"id":"4", "name":"Glasses", "description":"Best Glasses", "price": 20}
 ]
     
 class ProductIndexView(View):
@@ -47,3 +47,14 @@ class ProductShowView(View):
         viewData["subtitle"] = product["name"] + " - Product information"
         viewData["product"] = product
         return render(request, self.template_name, viewData)
+    
+class ContactPageView(TemplateView):
+    template_name = "pages/contacts.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact Us - Online Store",
+            "subtitle": "Contact Information",
+        })
+        return context
